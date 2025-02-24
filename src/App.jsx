298d44558f75266a96aab8bf2ea9fad445387f21
Babuali7739 <129478089+Babuali7739@users.noreply.github.com>
2025-02-24 21:41:32 +0530

    m

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React,{useState}from 'react'
+import Navbar from './components/Navbar/Navbar';
+import Admin from './pages/Admin/Admin.jsx';
+
+const App = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme(prev => !prev); // Toggle theme
+  };
+  return (
+    <div className={`app ${isDarkTheme ? 'dark' : 'light'}`}>
+      <Navbar isDarkTheme={isDarkTheme} toggleTheme={toggleTheme} />
+
+      <Admin/>
+    </div>
+  )
+}
+
+export default App
